dc76231bb591ff04f1252bef30d3b9c8235451e4
Mortari <bmortari2000@gmail.com>
2019-11-10 10:56:32 -0300

    Começando a interface

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.scss';
+import { MdDashboard } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <div className="details">
-        Nome
-      </div>
-      <header></header>
+      <div className="details"></div>
+      <header>
+        <div className="user-info">
+          <p>Admin</p>
+          <div className="image"></div>
+        </div>
+      </header>
       <aside>
+        <span>Geral</span>
         <ul>
-          <li>dajdnajdnkjad</li>
+          <li>
+            <MdDashboard />
+            Dashboard
+          </li>
         </ul>
       </aside>
       <main></main>
